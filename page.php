cf76f05ab461aec0bd4aebf0cf650021d10e89fd
Gabriel Fleig Alves <fleigalves@hotmail.com>
2025-01-28 23:40:40 -0300

    parto dia 3 - page marromeno

--- page.php
+++ page.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
+<div class="imagem">
+    <?php
+    // Obtém a URL da imagem do Customizer
+    $imagem_banner_url = get_theme_mod('imagem_banner');
 
-<div class="corpo" id="conteudo_pagina">
+    if (!empty($imagem_banner_url)) {
+        echo '<img src="' . esc_url($imagem_banner_url) . '" alt="Imagem decorativa do site">';
+        /*echo '<div class="imagem" style="background-image: url("' . esc_url($imagem_banner_url) . '")" alt="Imagem decorativa do site"><div>';*/
+    }
+    ?>    
+</div> 
+<div class="corpo width-wrapper" id="conteudo_pagina">
     <div class="corpo-grid">
         <div class="sidebar">
               
@@ -69,9 +79,8 @@
                 echo '<img src="' . esc_url($imagem_banner_url) . '" alt="Imagem decorativa do site">';
                 /*echo '<div class="imagem" style="background-image: url("' . esc_url($imagem_banner_url) . '")" alt="Imagem decorativa do site"><div>';*/
             }
-            ?>
-            <div class="imagem-sombra"></div>
-        </div>    
+            ?>    
+        </div>
 
     </div>
 </div>
