3aa57321bb5bc83be88a453763c7da302661c3ac
Gabriel Fleig Alves <fleigalves@hotmail.com>
2025-05-22 12:13:32 -0300

    2.0.6 - Gradiente na imagem e Título do Site

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo get_bloginfo( 'name' ); ?> - UFPB</title>
+    <title><?php echo esc_html( get_the_title() ); ?></title>
     <?php wp_head(); ?>
 </head>
 <body>
