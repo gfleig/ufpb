b70068a8f681645a3a65dd7916e488a0c6f36cda
Gabriel Fleig Alves <fleigalves@hotmail.com>
2025-07-08 16:46:02 -0300

    2.1.4 - Pequenas alterações (evento-card)

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wdth,wght@0,75..100,100..700;1,75..100,100..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&display=swap" rel="stylesheet">
     
     <link href="<?php echo get_bloginfo("template_directory"); ?>/assets/fontawesome6/css/fontawesome.min.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo("template_directory"); ?>/assets/fontawesome6/css/brands.min.css" rel="stylesheet">
