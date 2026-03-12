6925247d87faf064670016f3dd2a2273b2d11ef0
Gabriel Fleig Alves <fleigalves@hotmail.com>
2026-03-12 11:04:55 -0300

    2.6.2_beta - Tags OpenGraph para Prévia no Whatsapp

--- functions.php
+++ functions.php
@@ -1395,5 +1395,37 @@ function get_sorted_categories( $order_by = 'date', $args = array() ){
     return $category;
 }
 
+//https://stackoverflow.com/questions/53862332/meta-ogimage-not-working-for-my-wordpress-website-using-ssl
+
+add_action('wp_head', 'fc_opengraph');
+
+function fc_opengraph() {
+    if( is_single() || is_page() ) {
+        $locale = get_locale();
+        $post_id = get_queried_object_id();
+        $url = get_permalink($post_id);
+        $title = get_the_title($post_id);
+        $site_name = get_bloginfo('name');
+        $description = get_the_excerpt($post_id); 
+        //$description = wp_trim_words( get_post_field('post_content', $post_id), 25 );
+        if ( has_post_thumbnail($post_id )) {
+            $image = get_the_post_thumbnail_url($post_id);
+        } else {
+            $image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' )[0];
+        }
+        
+        
+
+        echo '<meta property="og:locale" content="' . esc_attr($locale) . '" />';
+        echo '<meta property="og:type" content="article" />';
+        echo '<meta property="og:title" content="' . esc_attr($title) . '" />';
+        echo '<meta property="og:description" content="' . esc_attr($description) . '" />';
+        echo '<meta property="og:url" content="' . esc_url($url) . '" />';
+        echo '<meta property="og:site_name" content="' . esc_attr($site_name) . '" />';
+        if($image) echo '<meta property="og:image" content="' . esc_url($image) . '" />';
+        
+    }
+}
+
 
 ?>
